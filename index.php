f9edd10d6dec807d373238b9fe92a63241fdf113
marco <lazzari.marco.ml@gmail.com>
2024-03-19 15:35:18 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+function generatePassWord($length)
+{
+    // possible letters and symbols that can end up in the pw, we don't need a variable for numbers because of random_int
+    $seed_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $seed_letters_length = strlen($seed_letters);
+    $seed_symbols = '!@#$%^&*()';
+    $seed_symbols_length = strlen($seed_symbols);
+    // variable for our output
+    $random_string = '';
+
+    // i tied to pw length chosen by user
+    for ($i = 0; $i < $length; $i++) {
+        // randomly picks between letter, symbol, number for each character
+        switch (random_int(1, 3)) {
+            case 1:
+                $random_letter = $seed_letters[random_int(0, $seed_letters_length - 1)];
+                $random_string .= $random_letter;
+                break;
+            case 2:
+                $random_symbol = $seed_symbols[random_int(0, $seed_symbols_length - 1)];
+                $random_string .= $random_symbol;
+                break;
+            case 3:
+                $random_string .= random_int(0, 9);
+                break;
+        }
+    }
+
+    return $random_string;
+}
+
+
+var_dump(generatePassWord(15));
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body data-bs-theme="dark">
+
+    <form action="">
+        <label for="pw-length">How long should your password be?</label>
+        <input type="number" name="pw-length" id="pw-length">
+
+    </form>
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
