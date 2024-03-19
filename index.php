f1c7b4a0ee7b68416f9458f20192c347c1b57031
marco <lazzari.marco.ml@gmail.com>
2024-03-19 17:51:54 +0100

    aggiunti checkbox che permettono all'utente di decidere i caratteri della password

--- index.php
+++ index.php
@@ -4,20 +4,37 @@ session_start();
 require "./partials/functions.php";
 
 if(isset($_GET['pw-length'])){
-    $_SESSION["userpw"] = generatePassWord($_GET['pw-length']);
+    $_SESSION["userpw"] = generatePassWord($_GET['pw-length'], $_GET['letters'], $_GET['numbers'], $_GET['symbols']);
     header('Location: ./form_output.php');
 }
 
 require "./partials/head.php";
+
 ?>
 
 
-
     <div class="container vh-100 d-flex flex-column align-items-center justify-content-center">
-        <form action="">
-            <label for="pw-length">How long should your password be?</label>
-            <input type="number" name="pw-length" id="pw-length">
-            <button type="submit" class="btn btn-outline-light">Confirm</button>
+        <form action="index.php" class="d-flex flex-column gap-2">
+            <div>
+                <label for="pw-length">How long should your password be?</label>
+                <input type="number" name="pw-length" id="pw-length">
+                <button type="submit" class="btn btn-outline-light">Confirm</button>
+            </div>
+
+            <div class="d-flex justify-content-center align-items-center gap-4">
+                <div>
+                    <label for="letters">Letters</label>
+                    <input type="checkbox" name="letters" id="letters">                    
+                </div>
+                <div>
+                    <label for="numbers">Numbers</label>
+                    <input type="checkbox" name="numbers" id="numbers">                    
+                </div>
+                <div>
+                    <label for="symbols">Symbols</label>
+                    <input type="checkbox" name="symbols" id="symbols">                    
+                </div>
+            </div>
         </form>
     </div>
 
--- partials/functions.php
+++ partials/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function generatePassWord($length)
+function generatePassWord($length, $letters = null, $numbers = null, $symbols = null,)
 {
     // possible letters and symbols that can end up in the pw, we don't need a variable for numbers because of random_int
     $seed_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -9,20 +9,35 @@ function generatePassWord($length)
     $seed_symbols_length = strlen($seed_symbols);
     // variable for our output
     $random_string = '';
+    
+    $valid_parameters = [];
 
+    if(isset($letters)){
+        array_push($valid_parameters, 'letters');
+    }
+
+    if(isset($numbers)){
+        array_push($valid_parameters, 'numbers');
+    }
+
+    if(isset($symbols)){
+        array_push($valid_parameters, 'symbols');
+    }
+    
     // i tied to pw length chosen by user
     for ($i = 0; $i < $length; $i++) {
+        
         // randomly picks between letter, symbol, number for each character
-        switch (random_int(1, 3)) {
-            case 1:
+        switch ($valid_parameters[random_int(0, count($valid_parameters) - 1)]) {
+            case 'letters':
                 $random_letter = $seed_letters[random_int(0, $seed_letters_length - 1)];
                 $random_string .= $random_letter;
                 break;
-            case 2:
+            case 'symbols':
                 $random_symbol = $seed_symbols[random_int(0, $seed_symbols_length - 1)];
                 $random_string .= $random_symbol;
                 break;
-            case 3:
+            case 'numbers':
                 $random_string .= random_int(0, 9);
                 break;
         }
